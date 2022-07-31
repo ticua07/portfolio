831768992225165405ea7f2d5b16330bbe1dd0ca
Ticua <ticua@ticua.ar>
2022-07-31 03:39:45 -0300

    Added little easter egg (Touch the cat!)

--- sections/Introduction.tsx
+++ sections/Introduction.tsx
@@ -6,22 +6,24 @@ import styles from "../styles/Introduction.module.css";
 const Introduction: NextPage = () => {
   const player = useRef(null);
 
-  useEffect(() => {
-    // @ts-ignore
-    player.current.src = "../public/Meow - Sound Effect-6wvFozENHb8.mp3";
-    // player.current.play();
-  }, []);
+  // useEffect(() => {
+  //   // @ts-ignore
+  //   // player.current.src = "../public/Meow - Sound Effect-6wvFozENHb8.mp3";
+  //   // player.current.play();
+  // }, []);
 
   return (
     <div className={styles.container}>
-      <audio ref={player} />
+      <audio ref={player} src="/Meow - Sound Effect-6wvFozENHb8.mp3" />
       <div>
         <Image
           onClick={() => {
+            // @ts-ignore
             player.current.play();
+            alert("Hey! What are you doing?");
           }}
           className={styles.pfp}
-          src="/../public/pfp.webp"
+          src="/pfp.webp"
           alt="profile"
           height={192}
           width={192}
