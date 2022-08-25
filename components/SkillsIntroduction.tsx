f9e4efdc86a70ad378283a6b18d65dad9e6d7828
Ticua <ticua@ticua.ar>
2022-08-25 17:04:37 -0300

    Added Skills introduction text back

---
+++ components/SkillsIntroduction.tsx
@@ -0,0 +1,20 @@
+import styles from "../styles/SkillsIntro.module.css";
+
+export default function SkillsIntroduction() {
+  return (
+    <div className={styles.container}>
+      <div className={styles.title__container}>
+        <h2 className={styles.title}>My Skills</h2>
+      </div>
+      <div className={styles.text__container}>
+        <p className={styles.about__text}>
+          People used to use skillbars to show their amazing skills
+          <br />
+          <span className={styles.keyword}>That time is over</span>,{" "}
+          <i>Have a look at actual real projects.</i>
+        </p>
+      </div>
+      <hr style={{ width: "100%" }} />
+    </div>
+  );
+}
--- sections/Skills.tsx
+++ sections/Skills.tsx
@@ -5,6 +5,7 @@ import { useEffect } from "react";
 import SkillBar from "../components/SkillBar";
 import "aos/dist/aos.css";
 import styles from "../styles/Skills.module.css";
+import SkillsIntroduction from "../components/SkillsIntroduction";
 
 const Skills: NextPage = () => {
   useEffect(() => {
@@ -13,72 +14,77 @@ const Skills: NextPage = () => {
   });
 
   return (
-    <div className={styles.container}>
-      <a
-        href="https://github.com/ticua07/ml-scraper-backend"
-        rel="noreferrer"
-        target="_blank"
-      >
-        <div className={styles.card}>
-          <div className={styles.cardImage}>
-            <section className={styles.content}>
-              <h1 className={styles.project__title}>MELI Scraper</h1>
-              <div className={styles.project__text}>
-                <p>
-                  Web scraper with UI, for looking at products in Mercado Libre
-                </p>
-                <p>Some of it&apos;s features are:</p>
-                <ul>
-                  <li>
-                    Products are saved and restored when closing and opening the
-                    website.
-                  </li>
-                  <li>Shows if discounts are available.</li>
-                  <li>Mobile-friendly design with working responsiveness.</li>
-                </ul>
+    <>
+      <SkillsIntroduction />
+      <div className={styles.container}>
+        <a
+          href="https://github.com/ticua07/ml-scraper-backend"
+          rel="noreferrer"
+          target="_blank"
+        >
+          <div className={styles.card}>
+            <div className={styles.cardImage}>
+              <section className={styles.content}>
+                <h1 className={styles.project__title}>MELI Scraper</h1>
+                <div className={styles.project__text}>
+                  <p>
+                    Web scraper with UI, for looking at products in Mercado
+                    Libre
+                  </p>
+                  <p>Some of it&apos;s features are:</p>
+                  <ul>
+                    <li>
+                      Products are saved and restored when closing and opening
+                      the website.
+                    </li>
+                    <li>Shows if discounts are available.</li>
+                    <li>Mobile-friendly design with working responsiveness.</li>
+                  </ul>
 
-                <p>
-                  Made with ReactJS and Fastapi. Used to be deployed on Heroku
-                  and Vercel
-                </p>
-              </div>
-            </section>
+                  <p>
+                    Made with ReactJS and Fastapi. Used to be deployed on Heroku
+                    and Vercel
+                  </p>
+                </div>
+              </section>
+            </div>
           </div>
-        </div>
-      </a>
-      <a
-        href="https://github.com/ticua07/ml-scraper-backend"
-        rel="noreferrer"
-        target="_blank"
-      >
-        <div className={styles.card}>
-          <div className={styles.cardImage}>
-            <section className={styles.content}>
-              <h1 className={styles.project__title}>MELI Scraper</h1>
-              <div className={styles.project__text}>
-                <p>
-                  Web scraper with UI, for looking at products in Mercado Libre
-                </p>
-                <p>Some of it&apos;s features are:</p>
-                <ul>
-                  <li>
-                    Products are saved and restored when closing and opening the
-                    website.
-                  </li>
-                  <li>Shows if discounts are available.</li>
-                  <li>Mobile-friendly design with working responsiveness.</li>
-                </ul>
+        </a>
+        <a
+          href="https://github.com/ticua07/ml-scraper-backend"
+          rel="noreferrer"
+          target="_blank"
+        >
+          <div className={styles.card}>
+            <div className={styles.cardImage}>
+              <section className={styles.content}>
+                <h1 className={styles.project__title}>MELI Scraper</h1>
+                <div className={styles.project__text}>
+                  <p>
+                    Web scraper with UI, for looking at products in Mercado
+                    Libre
+                  </p>
+                  <p>Some of it&apos;s features are:</p>
+                  <ul>
+                    <li>
+                      Products are saved and restored when closing and opening
+                      the website.
+                    </li>
+                    <li>Shows if discounts are available.</li>
+                    <li>Mobile-friendly design with working responsiveness.</li>
+                  </ul>
 
-                <p>
-                  Made with ReactJS and Fastapi. Used to be deployed on Heroku
-                  and Vercel
-                </p>
-              </div>
-            </section>
+                  <p>
+                    Made with ReactJS and Fastapi. Used to be deployed on Heroku
+                    and Vercel
+                  </p>
+                </div>
+              </section>
+            </div>
           </div>
-        </div>
-      </a>
-    </div>
+        </a>
+      </div>
+    </>
   );
 };
 
